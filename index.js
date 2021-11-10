b0a9385f5b33e2c12f50002b8c6fb3d9389d6f44
salamandaaa <hht2010ily@hotmail.com>
2021-11-10 16:23:41 +0800

    add commander

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
 const { program } = require('commander');
+const kitty = require('./kitty');
+
+function list(val) {
+    return val.split(',');
+}
 
 async function main() {
-	program
-	  .version('0.1.0')
-	  .option('-s, --send', 'send transaction')
-	  .option('-n, --nonce', 'nonce for first transaction')
-	  .option('-p, --price', 'gas price for transaction')
-	  .parse(process.argv);
-	console.log(program.opts())
+    program
+      .version('0.1.0')
+      .option('--set_sale_auction_address <contract-address>', 'Set sale auction address')
+	  .option('--set_sire_auction_address <contract-address>', 'Set siring auction address')
+	  .option('--set_gene_science_address <contract-address>', 'Set gene science address')
+	  .option('--unpause', 'Unpause the game')
+	  .option('--airdrop <receiver>', 'Airdrop kitty to address')
+	  .option('--owner_of <id>', 'Get the owner of a kitty')
+	  .option('--kitties_of_owner <owner>', 'Get kitties of an owner')
+	  .option('--create_kitty', 'Create auction for kitty of generation 0')
+	  .option('--query_sale_auction <id>', 'Query sale auction of a kitty')
+	  .option('--bid_for_sale <id, value, bidder>', 'Bid for buying kitty', list)
+	  .option('--sell_kitty <id, begin-price, end-price, duration(s), seller>', 'Sell a kitty, the operation will craete a sale auction', list)
+	  .option('--sire_kitty <id, begin-price, end-price, duration(s), owner>', 'Sire a kitty, the operation will create a sire auction', list)
+	  .option('--query_sire_auction <id>', 'Query sire auction of a kitty')
+	  .option('--bid_for_sire <sire-id, matron-id, value, bidder>', 'Bid for siring a kitty', list)
+	  .option('--give_birth <id, caller>', 'Give birth to a kitty', list)
+	  .option('--ready_to_breed <id>', 'Is a kitty ready to breed')
+      .parse(process.argv);
 
-	if (program.opts().send) {
-		console.log('signTransaction');
-		sendTransaction();
-	}
-	else if (program.opts().nonce) {
-		let nonce = await util.getNonce();
-		console.log(nonce);
-	}
-	else if (program.opts().price) {
-		console.log(await util.getGasPrice())
-	}
+    if (program.opts().set_sale_auction_address) {
+		kitty.setSaleAuctionAddress(program.opts().set_sale_auction_address);
+    }
+    else if (program.opts().set_sire_auction_address) {
+        kitty.setSiringAuctionAddress(program.opts().set_sire_auction_address);
+    }
+    else if (program.opts().set_gene_science_address) {
+        kitty.setGeneScienceAddress(program.opts().set_gene_science_address);
+    }
+    else if (program.opts().unpause) {
+        kitty.unpause();
+    }
+    else if (program.opts().airdrop) {
+        kitty.airdrop(program.opts().airdrop);
+    }
+    else if (program.opts().owner_of) {
+        kitty.ownerOf(program.opts().owner_of);
+    }
+    else if (program.opts().kitties_of_owner) {
+        kitty.tokensOfOwner(program.opts().kitties_of_owner);
+    }
+    else if (program.opts().create_kitty) {
+        kitty.createGen0Auction();
+    }
+    else if (program.opts().query_sale_auction) {
+        kitty.queryKittyAuction(program.opts().query_sale_auction);
+    }
+    else if (program.opts().bid_for_sale) {
+        let args = program.opts().bid_for_sale;
+        kitty.bidForKitty(args[0], args[1], args[2]);
+    }
+    else if (program.opts().sell_kitty) {
+        let args = program.opts().sell_kitty;
+        kitty.sellKitty(args[0], args[1], args[2], args[3], args[4]);
+    }
+    else if (program.opts().sire_kitty) {
+        let args = program.opts().sire_kitty;
+        kitty.sireKitty(args[0], args[1], args[2], args[3], args[4]);
+    }
+    else if (program.opts().query_sire_auction) {
+        kitty.querySireAuction(program.opts().query_sire_auction);
+    }
+    else if (program.opts().bid_for_sire) {
+        let args = program.opts().bid_for_sire;
+        kitty.bidForSire(args[0], args[1], args[2], args[3]);
+    }
+    else if (program.opts().give_birth) {
+        let args = program.opts().give_birth;
+        kitty.giveBirth(args[0], args[1]);
+    }
+    else if (program.opts().ready_to_breed) {
+        kitty.isReadyToBreed(program.opts().ready_to_breed);
+    }
 }
 
 main()
--- kitty.js
+++ kitty.js
@@ -2,11 +2,11 @@ const Web3 = require('web3');
 const config = require('./config');
 
 // setSaleAuctionAddress
-function setSaleAuctionAddress(auctionAddress) {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.setSaleAuctionAddress = function(auctionAddress) {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.setSaleAuctionAddress(auctionAddress).send({
@@ -18,11 +18,11 @@ function setSaleAuctionAddress(auctionAddress) {
 }
 
 // setSiringAuctionAddress
-function setSiringAuctionAddress(auctionAddress) {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.setSiringAuctionAddress = function(auctionAddress) {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.setSiringAuctionAddress(auctionAddress).send({
@@ -34,11 +34,11 @@ function setSiringAuctionAddress(auctionAddress) {
 }
 
 // setGeneScienceAddress
-function setGeneScienceAddress(geneAddress) {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.setGeneScienceAddress = function(geneAddress) {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.setGeneScienceAddress(geneAddress).send({
@@ -50,11 +50,11 @@ function setGeneScienceAddress(geneAddress) {
 }
 
 // unpause
-function unpause() {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.unpause = function() {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.unpause().send({
@@ -66,11 +66,11 @@ function unpause() {
 }
 
 // airdrop
-function airdrop(owner) {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.airdrop = function(owner) {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.createPromoKitty(ts, owner).send({
@@ -82,25 +82,25 @@ function airdrop(owner) {
 }
 
 // ownerOf
-function ownerOf(tokenId) {
-	web3 = new Web3(config.RPC);
+module.exports.ownerOf = function(tokenId) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.ownerOf(tokenId).call(null, (error, result) => console.log(result));
 }
 
 // tokensOfOwner
-function tokensOfOwner(owner) {
-	web3 = new Web3(config.RPC);
+module.exports.tokensOfOwner = function(owner) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.tokensOfOwner(owner).call(null, (error, result) => console.log(result));
 }
 
 // createGen0Auction
-function createGen0Auction() {
-	// get gene
-	let ts = new Date().getTime();
-	// create kitty
-	web3 = new Web3(config.RPC);
+module.exports.createGen0Auction = function() {
+    // get gene
+    let ts = new Date().getTime();
+    // create kitty
+    web3 = new Web3(config.RPC);
     let from = config.OWNER;
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.createGen0Auction(ts).send({
@@ -112,15 +112,15 @@ function createGen0Auction() {
 }
 
 // query auction for selling kitty
-function queryKittyAuction(tokenId) {
-	web3 = new Web3(config.RPC);
+module.exports.queryKittyAuction = function(tokenId) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.SALE_CLOCK_AUCTION_API), config.SALE_CLOCK_AUCTION_ADDRESS, {net_type: 'lat'});
     contract.methods.getAuction(tokenId).call(null, (error, result) => console.log(result));
 }
 
 // bidForKitty
-function bidForKitty(tokenId, lat, user) {
-	web3 = new Web3(config.RPC);
+module.exports.bidForKitty = function(tokenId, lat, user) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.SALE_CLOCK_AUCTION_API), config.SALE_CLOCK_AUCTION_ADDRESS, {net_type: 'lat'});
     contract.methods.bid(tokenId).send({
         from: user,
@@ -132,8 +132,8 @@ function bidForKitty(tokenId, lat, user) {
 }
 
 // sellKitty
-function sellKitty(tokenId, startPrice, endPrice, duration, seller) {
-	web3 = new Web3(config.RPC);
+module.exports.sellKitty = function(tokenId, startPrice, endPrice, duration, seller) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.createSaleAuction(tokenId, startPrice, endPrice, duration).send({
         from: seller,
@@ -144,8 +144,8 @@ function sellKitty(tokenId, startPrice, endPrice, duration, seller) {
 }
 
 // sireKitty
-function sireKitty(tokenId, startPrice, endPrice, duration, owner) {
-	web3 = new Web3(config.RPC);
+module.exports.sireKitty = function(tokenId, startPrice, endPrice, duration, owner) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.createSiringAuction(tokenId, startPrice, endPrice, duration).send({
         from: owner,
@@ -156,15 +156,15 @@ function sireKitty(tokenId, startPrice, endPrice, duration, owner) {
 }
 
 // query auction for siring kitty
-function querySireAuction(tokenId) {
-	web3 = new Web3(config.RPC);
+module.exports.querySireAuction = function(tokenId) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.SIRING_CLOCK_AUCTION_API), config.SIRING_CLOCK_AUCTION_ADDRESS, {net_type: 'lat'});
     contract.methods.getAuction(tokenId).call(null, (error, result) => console.log(result));
 }
 
 // bidForSire
-function bidForSire(sireId, matronId, lat, user) {
-	web3 = new Web3(config.RPC);
+module.exports.bidForSire = function(sireId, matronId, lat, user) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.bidOnSiringAuction(sireId, matronId).send({
         from: user,
@@ -176,8 +176,8 @@ function bidForSire(sireId, matronId, lat, user) {
 }
 
 // giveBirth
-function giveBirth(matronId, user) {
-	web3 = new Web3(config.RPC);
+module.exports.giveBirth = function(matronId, user) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.giveBirth(matronId).send({
         from: user,
@@ -188,28 +188,8 @@ function giveBirth(matronId, user) {
 }
 
 // isReadyToBreed
-function isReadyToBreed(tokenId) {
-	web3 = new Web3(config.RPC);
+module.exports.isReadyToBreed = function(tokenId) {
+    web3 = new Web3(config.RPC);
     let contract = new web3.platon.Contract(JSON.parse(config.KITTY_CORE_API), config.KITTY_CORE_ADDRESS, {net_type: 'lat'});
     contract.methods.isReadyToBreed(tokenId).call(null, (error, result) => console.log(result));
 }
-
-
-// setSaleAuctionAddress(config.SALE_CLOCK_AUCTION_ADDRESS);
-// setSiringAuctionAddress(config.SIRING_CLOCK_AUCTION_ADDRESS);
-// setGeneScienceAddress(config.GENE_ADDRESS);
-// unpause();
-// airdrop(config.USER2);
-// ownerOf(1);
-// tokensOfOwner(config.SALE_CLOCK_AUCTION_ADDRESS);
-// createGen0Auction();
-// queryKittyAuction(1);
-// bidForKitty(1, config.USER1);
-// tokensOfOwner(config.USER2);
-// sellKitty(1, '1000000000000000000', '100000000000000000', 86400, config.USER1);
-// bidForKitty(1, '1000000000000000000', config.USER2);
-// sireKitty(1, '1000000000000000000', '100000000000000000', 86400, config.USER1);
-// querySireAuction(1);
-// bidForSire(1, 2, '1000000000000000000', config.USER2);
-// giveBirth(2, config.USER1);
-isReadyToBreed(2);
